310854ef5247ad5942da2f6ba5009a368af56180
BatmanVN <vumanhlinh16111995@gmail.com>
2024-08-30 19:34:20 +0700

    Begin Set map 2

--- Assets/_MyAssets/Script/Indicator/GunIndicator.cs
+++ Assets/_MyAssets/Script/Indicator/GunIndicator.cs
@@ -4,20 +4,31 @@ using UnityEngine;
 
 public class GunIndicator : MonoBehaviour
 {
-    [SerializeField] private GameObject[] indicator;
-    [SerializeField] private Transform enemy;
+    [SerializeField] private List<GameObject> indicators;
+
+    private void Start()
+    {
+        AddIndicator();
+    }
+    private void AddIndicator()
+    {
+        foreach (Transform child in transform)
+        {
+            indicators.Add(child.gameObject);
+        }
+    }
 
     public void EnableWhiteIndicator()
     {
-        indicator[0].SetActive(true);
-        indicator[1].SetActive(false);
+        indicators[0].SetActive(true);
+        indicators[1].SetActive(false);
     }
 
     public void EnableRedIndicator()
     {
-        indicator[0].SetActive(false);
-        indicator[1].SetActive(true);
+        indicators[0].SetActive(false);
+        indicators[1].SetActive(true);
     }
 
-    public void DisableWhiteIndicator() => indicator[0].SetActive(false);
+    public void DisableWhiteIndicator() => indicators[0].SetActive(false);
 }
