16394b5ec7c4fc3e13e6580c4dded5a0b662b635
Thanhnx <thanhnx119@gmail.com>
2025-01-24 16:20:24 +0700

    Thêm logic move cơ bản

--- Assets/_Thanhnx/Scripts/BasePlayer.cs
+++ Assets/_Thanhnx/Scripts/BasePlayer.cs
@@ -1,18 +1,37 @@
 using System;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class BasePlayer : BaseCharacter
 {
+    [SerializeField]
+    private VariableJoystick _joystick;
     
     [SerializeField]
-    private float moveSpeed;
+    private Rigidbody _rb;
+    
+    [Header("Move")]
+    [SerializeField]
+    private float _moveSpeed;
+    
+    [Header("Anim")]
+    [SerializeField]
+    private Animator _animator;
 
     private void FixedUpdate()
     {
-        
+        Moving();
     }
-    private MovingUp()
+    private void Moving()
     {
+        var horizontal = _joystick.Horizontal;
+        var vertical = _joystick.Vertical;
         
+        _rb.linearVelocity = new Vector3(horizontal * _moveSpeed, _rb.linearVelocity.y, vertical * _moveSpeed);
+
+        transform.rotation = Quaternion.LookRotation(new Vector3(horizontal, 0, vertical));
+        
+        float speed = new Vector2(horizontal, vertical).magnitude;
+        _animator.SetFloat("Speed", speed);
     }
 }
